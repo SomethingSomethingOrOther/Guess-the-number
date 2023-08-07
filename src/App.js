7b4a510492003b722471007a7e11bc11a03286c2
SomethingSomethingOrOther <abdoulgdia@gmail.com>
2023-08-07 14:31:51 -0400

    first guess the number commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react"
+
 
 function App() {
+  const [inputValue,setInputValue]=useState("")
+  const [hidden,setHidden]=useState(60)
+  const [message,setMessage]=useState("")
+
+     const handleChange=()=>{
+          if (inputValue === hidden){
+            console.log("That is correct")
+            setMessage("This is correct")
+          }else if (inputValue > hidden){
+            console.log("Your Number is GREATER THAN the actual number")
+            setMessage("Your Number is GREATER THAN the actual number")
+          }else {
+            console.log("Your number is LESS THAN the actual number")
+            setMessage("Your number is LESS THAN the actual number")
+          }
+     } 
+
+     const handleReset=()=>{
+        if (hidden <= 100){
+          setHidden(hide=>hide + 10)
+        }else {
+          setHidden(hide=>hide-10)
+        }
+
+        setInputValue("")
+     }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Guess the Number</h1>
       </header>
+      <div>
+      Guess a Number between 0-100
+      </div>
+      <div>
+        <input value={inputValue} onChange={(e)=>setInputValue(Number(e.target.value))}type="text" />
+      </div>
+      <div>
+        <button onClick={handleReset}>Reset</button>
+        <button onClick={handleChange}>Check</button>
+      </div>
+      <div className="app-message">
+         {message}
+      </div>
+      
     </div>
   );
 }
